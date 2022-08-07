8d786c07bbf6a901c061c6a9d153df02d747e71f
Joey Shepard <joey.m.shepard@gmail.com>
2022-08-06 21:14:50 -0400

    moved to AS for removal of assembler optimizer dependency and NASM

--- emu.bat
+++ emu.bat
@@ -2,4 +2,4 @@
 
 REM - Copies binary to node.js emulator for testing calculations
 
-copy emu.hex "..\..\projects\6502 emu\node.js\prog.hex" > nul
+copy emu.hex "..\..\..\projects\6502 emu\node.js\prog.hex" > nul
--- run.bat
+++ run.bat
@@ -11,22 +11,22 @@ cd ..
 type main.err
 echo.
 del processed.asm
-python "..\..\projects\6502 Optimizer\NASM based\main.py" main.i
+python "..\..\..\projects\6502 Optimizer\NASM based\main.py" main.i
 type src\mem_template.asm >> processed.asm
 echo.
 echo Re-assembling...
-..\..\AS\bin\asw processed.asm -P -G -U -L -g -q -cpu 6502 > asm.txt
+..\..\bin\asw processed.asm -P -G -U -L -g -q -cpu 6502 > asm.txt
 python "remove escape.py" asm.txt
 echo Generating hex file...
-..\..\AS\bin\p2hex processed.p -F Intel -l 32 -r $0000-$FFFF > hex.txt
+..\..\bin\p2hex processed.p -F Intel -l 32 -r $0000-$FFFF > hex.txt
 copy processed.hex emu.hex > nul
 echo :02FFFC000009BF >> emu.hex
 echo Copying...
-copy processed.lst "..\..\projects\6502 emu\local copy\listing.lst" > nul
-REM copy processed.hex "..\..\projects\6502 emu\local copy\prog.hex" > nul
-copy emu.hex "..\..\projects\6502 emu\local copy\prog.hex" > nul
-REM copy emu.hex "..\..\projects\6502 emu\node.js\prog.hex" > nul
-copy input.txt "..\..\projects\6502 emu\local copy\input.txt" > nul
-REM break > "..\..\projects\6502 emu\local copy\keys.txt"
-copy keys.txt "..\..\projects\6502 emu\local copy\keys.txt" > nul
+copy processed.lst "..\..\..\projects\6502 emu\local copy\listing.lst" > nul
+REM copy processed.hex "..\..\..\projects\6502 emu\local copy\prog.hex" > nul
+copy emu.hex "..\..\..\projects\6502 emu\local copy\prog.hex" > nul
+REM copy emu.hex "..\..\..\projects\6502 emu\node.js\prog.hex" > nul
+copy input.txt "..\..\..\projects\6502 emu\local copy\input.txt" > nul
+REM break > "..\..\..\projects\6502 emu\local copy\keys.txt"
+copy keys.txt "..\..\..\projects\6502 emu\local copy\keys.txt" > nul
 echo.
